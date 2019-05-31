010e092a0eb63a99a530d3ec9e2080afeb3ce5af
Josh Auerbach <jauerbach@champlain.edu>
2019-05-31 10:49:46 -0400

    fixed variable names and made debug visualization work (needs to be in main thread)

--- pacman/emotion_detector.py
+++ pacman/emotion_detector.py
@@ -8,7 +8,6 @@ from keras.models import load_model
 import numpy as np
 from multiprocessing.pool import ThreadPool
 
-
 # parameters for loading data and images
 DETECTION_MODEL_PATH = '../FaceEmotion_ID/haarcascade_files/haarcascade_frontalface_default.xml'
 EMOTION_MODEL_PATH = '../FaceEmotion_ID/models/_mini_XCEPTION.106-0.65.hdf5'
@@ -48,11 +47,44 @@ class EmotionDetector:
         Probabilities are aligned with EMOTIONS.
         """
 
+        def draw_stuff(predictions, frame, face):
+            """Draw info to screen for debug purposes."""
+
+            if face is not None:
+                face_x, face_y, face_width, face_height = face
+
+            canvas = np.zeros((250, 300, 3), dtype="uint8")
+            frame_clone = frame.copy()
+
+
+            label = EMOTIONS[predictions.argmax()]
+
+            for (i, (emotion, prob)) in enumerate(zip(EMOTIONS, predictions)):
+                # construct the label text
+                text = "{}: {:.2f}%".format(emotion, prob * 100)
+                w = int(prob * 300)
+                cv2.rectangle(canvas, (7, (i * 35) + 5),
+                              (w, (i * 35) + 35), (0, 0, 255), -1)
+                cv2.putText(canvas, text, (10, (i * 35) + 23),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.45,
+                            (255, 255, 255), 2)
+    
+            if face is not None:
+                cv2.putText(frame_clone, label, (face_x, face_y - 10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+                cv2.rectangle(frame_clone, (face_x, face_y), (face_x + face_width, face_y + face_height),
+                              (0, 0, 255), 2)
+    
+            cv2.imshow('Webcam', frame_clone)
+            cv2.imshow("Probabilities", canvas) 
+
+
         def predict_helper():
             """Actually predict.
 
             Runs in own thread.
             """
+
             frame = self.camera.read()[1]
 
             # reading the frame
@@ -61,56 +93,35 @@ class EmotionDetector:
             faces = self.face_detection.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30),
                                                          flags=cv2.CASCADE_SCALE_IMAGE)
 
-            canvas = np.zeros((250, 300, 3), dtype="uint8")
-
-            preds = np.zeros(len(EMOTIONS))
-            label = ""
+            predictions = np.ones(len(EMOTIONS)) / len(EMOTIONS)
+            face = None
 
             if len(faces) > 0:
-                faces = sorted(faces, reverse=True, key=lambda x: (x[2] - x[0]) * (x[3] - x[1]))[0]
+                face = sorted(faces, reverse=True, key=lambda x: (x[2] - x[0]) * (x[3] - x[1]))[0]
 
-                (fX, fY, fW, fH) = faces
-                # Extract the ROI of the face from the grayscale image, resize it to a fixed 48x48 pixels, and then prepare
-                # the ROI for classification via the CNN
-                roi = gray[fY:fY + fH, fX:fX + fW]
-                roi = cv2.resize(roi, (48, 48))
-                roi = roi.astype("float") / 255.0
-                roi = img_to_array(roi)
-                roi = np.expand_dims(roi, axis=0)
+                (face_x, face_y, face_width, face_height) = face
+                # Extract the ROI of the face from the grayscale image, resize it to a fixed 48x48 pixels,
+                # and then prepare the ROI for classification via the CNN
+                region_of_interest = gray[face_y:face_y + face_height, face_x:face_x + face_width]
+                region_of_interest = cv2.resize(region_of_interest, (48, 48))
+                region_of_interest = region_of_interest.astype("float") / 255.0
+                region_of_interest = img_to_array(region_of_interest)
+                region_of_interest = np.expand_dims(region_of_interest, axis=0)
 
-                preds = self.emotion_classifier.predict(roi)[0]
-                label = EMOTIONS[preds.argmax()]
+                predictions = self.emotion_classifier.predict(region_of_interest)[0]
 
-            if self.debug:
-                frame_clone = frame.copy()
-
-                for (i, (emotion, prob)) in enumerate(zip(EMOTIONS, preds)):
-                    # construct the label text
-                    text = "{}: {:.2f}%".format(emotion, prob * 100)
-                    w = int(prob * 300)
-                    cv2.rectangle(canvas, (7, (i * 35) + 5),
-                                  (w, (i * 35) + 35), (0, 0, 255), -1)
-                    cv2.putText(canvas, text, (10, (i * 35) + 23),
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.45,
-                                (255, 255, 255), 2)
-                    cv2.putText(frame_clone, label, (fX, fY - 10),
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
-                    cv2.rectangle(frame_clone, (fX, fY), (fX + fW, fY + fH),
-                                  (0, 0, 255), 2)
-
-                cv2.imshow('your_face', frame_clone)
-                cv2.imshow("Probabilities", canvas)
-
-            return preds
+            return predictions, frame, face
 
         if self.async_result is None:
             self.async_result = self.pool.apply_async(predict_helper, ())
 
         elif self.async_result.ready():
-            self.current_prediction = self.async_result.get()
+            self.current_prediction, frame, face = self.async_result.get()
+            draw_stuff(self.current_prediction, frame, face)
             self.async_result = self.pool.apply_async(predict_helper)
 
         return self.current_prediction
+
 
 
     def close(self):
@@ -122,7 +133,7 @@ class EmotionDetector:
 if __name__ == "__main__":
     # simple test
 
-    detector = EmotionDetector(False)
+    detector = EmotionDetector(True)
 
 
     while True:
@@ -130,5 +141,6 @@ if __name__ == "__main__":
         print(EMOTIONS[predictions.argmax()], list(zip(EMOTIONS, predictions)))
         if (cv2.waitKey(1) & 0xFF == ord('q')):
             break
+        detector.async_result.wait()
 
     detector.close()
